3a1180f7e604123f1b1005b3b021cc1811d72971
Artem <87029756+magicpigomg@users.noreply.github.com>
2023-04-15 04:22:50 +0300

    Update r.py

--- r.py
+++ r.py
@@ -14,3 +14,19 @@ for i in values:
     res = requests.get(link, proxies=optionsProxy).text
 
     print('Ваш ip', res)
+
+    
+start_port = 9000
+end_port = 9005
+
+for i in range(5):
+    port = start_port + (i % (end_port - start_port))
+    proxyTor = "socks5://127.0.0.1:" + str(port)
+
+    optionsProxy = {"https": proxyTor}
+
+    link = "https://api.ipify.org"
+
+    res = requests.get(link, proxies=optionsProxy).text
+
+    print(f'Ваш ip - {res}\n')
